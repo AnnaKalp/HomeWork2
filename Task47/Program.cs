69c2574f2b607cb0fcfcd523dd209c7bc38e055b
Ann <annadiacova@gmail.com>
2022-10-09 02:08:45 +0300

    task47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -5,13 +5,38 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9*/
 
-Console.Clear();
- 
 Console.Write("Введите m: ");
-int m = int.Parse(Console.ReadLine()!);
+int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите n: ");
-int n = int.Parse(Console.ReadLine()!);
+int n = Convert.ToInt32(Console.ReadLine());
 
-int[,] array = new int[m, n];
+double[,] array = new double[m, n];
+
 FillArray(array);
 PrintArray(array);
+ 
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+void FillArray(double[,] array)
+{
+    Random generator = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
